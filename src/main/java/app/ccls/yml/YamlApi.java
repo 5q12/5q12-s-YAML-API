cb717c6d7adfd348d7676ccfd47a530b20ba3c8e
5q12 <130976362+5q12@users.noreply.github.com>
2024-07-16 16:02:53 +0200

    v1.0.0

---
+++ src/main/java/app/ccls/yml/YamlApi.java
@@ -0,0 +1,10 @@
+package app.ccls.yml;
+
+import net.fabricmc.api.ModInitializer;
+
+public class YamlApi implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("YamlApi Mod has been initialized!");
+    }
+}
